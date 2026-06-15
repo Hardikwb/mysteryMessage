89719b0034367d36d18b39e0921d2a5f76f5a8f1
Hardikwb <hghardikgupta1@gmail.com>
2026-06-15 23:48:08 +0530

    update dependencies and add proxy middleware for authentication redirection

--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,33 @@ declare global {
     promise: Promise<Connection> | null;
   };
 }
+import "next-auth";
+import { DefaultSession } from "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+    username?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+  }
+
+  interface Session {
+    user: {
+      _id?: string;
+      username?: string;
+      isVerified?: boolean;
+      isAcceptingMessage?: boolean;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    _id: string;
+    username?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+  }
+}
